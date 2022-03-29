f39c48ea3395ed757caea21757f003ae77f099bb
LinoVelardita <102514924+LinoVelardita@users.noreply.github.com>
2022-03-29 15:17:28 +0200

    Update HeuristicCycle.java

--- HeuristicCycle.java
+++ HeuristicCycle.java
@@ -101,7 +101,7 @@ public class HeuristicCycle {
     private void connectToElastic(){
         final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
         credentialsProvider.setCredentials(AuthScope.ANY,
-                new UsernamePasswordCredentials("tirocini", "dpZfBAXLF7qq438T"));
+                new UsernamePasswordCredentials("???", "???"));
         RestClientBuilder builder = RestClient.builder(new HttpHost("es.tirocini.netseven.it", 443, "https"))
                 .setHttpClientConfigCallback(new RestClientBuilder.HttpClientConfigCallback() {
                     @Override
