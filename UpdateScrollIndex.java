43357b9bdbf466b9e0fed274977a77fa6038976f
LinoVelardita <102514924+LinoVelardita@users.noreply.github.com>
2022-04-01 15:57:42 +0200

    Update UpdateScrollIndex.java

--- UpdateScrollIndex.java
+++ UpdateScrollIndex.java
@@ -52,7 +52,7 @@ public class UpdateScrollIndex {
         //connect to elastic
         final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
         credentialsProvider.setCredentials(AuthScope.ANY,
-                new UsernamePasswordCredentials("tirocini", "dpZfBAXLF7qq438T"));
+                new UsernamePasswordCredentials("?????", "?????"));
         RestClientBuilder builder = RestClient.builder(new HttpHost("es.tirocini.netseven.it", 443, "https"))
                 .setHttpClientConfigCallback(new RestClientBuilder.HttpClientConfigCallback() {
                     @Override
